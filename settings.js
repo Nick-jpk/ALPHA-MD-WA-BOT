a9baa08e224fb06c9c5dbfe2d6fa2e55c022153f
Nick-jpk <nickheavens52@gmail.com>
2025-05-23 14:09:55 +0300

    Nick settings.js

--- settings.js
+++ settings.js
@@ -12,9 +12,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
-global.ownernomer = '94784506970'
+global.owner = ['+254113940847']
+global.premium = ['+254113940847']
+global.ownernomer = '+254113940847'
 global.ownername = 'Achi Fernando'
 global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
 global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
